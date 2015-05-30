fe99560e601cc76ca77f3f52a7aef3b4205bff7f
Leonel Pacehco <Leonel@Leonels-Mac.local>
2015-05-30 12:03:55 -0500

    Initial Commit

---
+++ adaptiveWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  adaptiveWeather
+//
+//  Created by Leonel Pacehco on 5/30/15.
+//  Copyright (c) 2015 Leonel Pacheco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
